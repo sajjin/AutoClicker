054c6fb7e3f163a78f65e1e4f5c97266ca089598
sajjin <sajjinnijjar@gmail.com>
2022-07-03 20:19:57 -0700

    added gui

--- main.py
+++ main.py
@@ -3,28 +3,47 @@ import pyautogui
 import keyboard
 import threading
 import time
+import PySimpleGUI as sg
+sg.ChangeLookAndFeel('Dark')
 
-mouse_down = pyautogui.mouseDown(button='left') 
-mouse_up = pyautogui.mouseUp(button='left')
-
-def run(stop):
+def run(stop, click_time):
     while True:
         if stop():
             break
         time.sleep(.15)
-        pyautogui.drag(1, 0, .25, button='left')
-        pyautogui.drag(-1, 0, .25, button='left')
+        pyautogui.drag(1, 0, click_time, button='left')
+        pyautogui.drag(-1, 0, click_time, button='left')
 
-
-def main():
-    time.sleep(10)
-    stop_threads = False
-    t1 = threading.Thread(target = run, args =(lambda : stop_threads, ))
-    t1.start()
-    keyboard.wait("q")
+def GUI():
+    layout = [
+        [sg.Text('enter key that you want to start the program')],
+        [sg.InputText(key='-START-')],
+        [sg.Text('enter key that you want to stop the program')],
+        [sg.InputText(key='-STOP-')],
+        [sg.Text('enter how long you want the mouse to be pressed for in seconds')],
+        [sg.InputText(key='-TIME-')],
+        [sg.Submit()],
+        [sg.Cancel()],
+    ]
+    window = sg.Window('Mouse Drag', layout)
+    while True:
+        event, values = window.read()
+        if event in (None, 'Cancel'):
+            break
+        if event == 'Submit':
+            keyboard.wait(values['-START-'])
+            stop_threads = False
+            t1 = threading.Thread(target = run, args =(lambda : stop_threads,  int(values['-TIME-'])))
+            t1.start()
+            keyboard.wait(values['-STOP-'])
+            break
     stop_threads = True
     t1.join()
     print('thread killed')
+    window.close()
+
+def main():
+    GUI()
 
 if __name__ == '__main__':
     main()
